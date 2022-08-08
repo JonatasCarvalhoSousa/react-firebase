38e64c00fa4515adcf0cb14a0d5e6618f150669e
Jônatas <jonatascarvalhodesousa@gmail.com>
2022-08-08 15:20:03 -0300

    Implementado acesso, cadastro e tela de admin

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,25 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./home.css";
+import { auth } from "../../firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  function handleLogin(e) {
+  async function handleLogin(e) {
     e.preventDefault();
 
     if (email !== "" && password !== "") {
-      alert("Foi");
+      await signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          navigate("/admin", { replace: true });
+        })
+        .catch(() => {
+          console.log("ERRO AO FAZER LOGIN");
+        });
     } else {
       alert("Preencha todos os campos");
     }
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./../Home/home.css";
+import { auth } from "../../firebaseConnection";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function handleCadastrar(e) {
+    e.preventDefault();
+
+    if (email !== "" && password !== "") {
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          navigate("/admin", { replace: true });
+        })
+        .catch(() => {
+          console.log("ERRO AO FAZER LOGIN");
+        });
+    } else {
+      alert("Preencha todos os campos");
+    }
+  }
+
   return (
-    <div>
-      <h1>Página Register</h1>
+    <div className="home-container">
+      <h1>Cadastre-se!</h1>
+      <span>Crie aqui sua conta</span>
+      <form className="form" onSubmit={handleCadastrar}>
+        <input
+          type="texrt"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          autoComplete={false}
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Cadastrar</button>
+      </form>
+      <Link className="button-link" to="/">
+        {" "}
+        Já possui uma conta ? Entre!
+      </Link>
     </div>
   );
 }
