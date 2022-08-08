fb358e18670660da56b236a4008b22dfc90c9f5d
Jônatas <jonatascarvalhodesousa@gmail.com>
2022-08-08 13:00:57 -0300

    Criado tela de login e estilizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
+
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email !== "" && password !== "") {
+      alert("Foi");
+    } else {
+      alert("Preencha todos os campos");
+    }
+  }
+
   return (
-    <div>
-      <h1>Página Home</h1>
+    <div className="home-container">
+      <h1>Lista de Tarefas</h1>
+      <span>Gerencie sua agenda de forma fácil.</span>
+      <form className="form" onSubmit={handleLogin}>
+        <input
+          type="texrt"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          autoComplete={false}
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Acessar</button>
+      </form>
+      <Link className="button-link" to="/register">
+        {" "}
+        Não possui uma conta ? Cadastre-se
+      </Link>
     </div>
   );
 }
