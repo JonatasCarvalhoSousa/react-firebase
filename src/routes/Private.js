431a46cb871e5475bad2682723cdaa1b41d70de9
Jônatas <jonatascarvalhodesousa@gmail.com>
2022-08-08 18:01:49 -0300

    Tornando tela admin privada apenas para usuários logados

---
+++ src/routes/Private.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { auth } from "../firebaseConnection";
+import { onAuthStateChanged } from "firebase/auth";
+import { async } from "@firebase/util";
+
+import { Navigate } from "react-router-dom";
+
+export default function Private({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [singed, setSingned] = useState(false);
+
+  useEffect(() => {
+    async function checkLogin() {
+      await onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const userData = {
+            uid: user.uid,
+            email: user.email,
+          };
+
+          localStorage.setItem("@detailUser", JSON.stringify(userData));
+
+          setLoading(false);
+          setSingned(true);
+        } else {
+          setLoading(false);
+          setSingned(false);
+        }
+      });
+    }
+    checkLogin();
+  }, []);
+
+  if (loading) {
+    return <div></div>;
+  }
+
+  if (!singed) {
+    return <Navigate to="/" />;
+  }
+  return children;
+}
